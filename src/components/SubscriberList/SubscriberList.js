0a7fb239f0f4a8161cef3a8febe96be924b8b0a7
Jakub Prymak <jprymak@gmail.com>
2021-11-15 21:21:21 +0100

    feat: add subscriber list that fetches and presents data

--- src/components/SubscriberList/SubscriberList.js
+++ src/components/SubscriberList/SubscriberList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function SubscriberList(props) {
+
+function SubscriberList({subscribers}) {
+    console.log(subscribers)
     return (
         <div>
-            
+            <ul>
+                {subscribers.map(({fields,id})=><li key={id}>{fields.Name}; {fields.Email}</li>)}
+            </ul>
         </div>
     );
 }
--- src/pages/Subscribers.js
+++ src/pages/Subscribers.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {useState, useEffect} from "react";
 
-function Subscribers(props) {
+import api from "../api";
+
+import {SubscriberList} from "../components/SubscriberList";
+
+function Subscribers() {
+const [subscribers, setSubscribers] = useState([]);
+const [isLoading, setIsLoading] = useState(true);
+const [hasError, setHasError] = useState(false);
+useEffect(()=>{
+api.get("https://api.airtable.com/v0/appfrBxpRB3whj4LA/Table%201")
+.then(dbData=>{
+    setSubscribers(dbData.records);
+    setIsLoading(false);
+})
+.catch(error=>{
+    console.log(error);
+    setHasError(true);
+})
+},[])
     return (
         <div>
-            Subscribers
+            {!isLoading && <SubscriberList subscribers={subscribers}/>}
         </div>
     );
 }
