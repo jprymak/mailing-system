d1e7147ac428c7d15058100e90464b7dc24a6a98
Jakub Prymak <jprymak@gmail.com>
2021-11-17 18:19:23 +0100

    chore: change variable name

--- src/pages/AddSubscriber.js
+++ src/pages/AddSubscriber.js
@@ -2,15 +2,15 @@ import React, {useState} from 'react';
 
 
 import {AddSubscriberForm} from '../components/AddSubscriberForm';
-import {createRecord} from "../utils";
-import api from "../api";
+import {createSubscriberRecord} from "../utils";
+import {api} from "../api";
 
 function AddSubscriber(props) {
     const [hasError, setHasError] = useState(false);
 
     const onSubmit = (data) =>{
         const {name, email} = data;
-        api.post("https://api.airtable.com/v0/appfrBxpRB3whj4LA/Table%201", createRecord(name, email))
+        api.post("https://api.airtable.com/v0/appfrBxpRB3whj4LA/Table%201", createSubscriberRecord(name, email))
         .catch(error=>{
             console.log(error);
             setHasError(true);
