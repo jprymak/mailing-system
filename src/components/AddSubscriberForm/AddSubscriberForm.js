171dbe5c3b1ea36215419b2fa0ec67a4b8129193
Jakub Prymak <jprymak@gmail.com>
2021-11-18 18:51:40 +0100

    build: add proptypes syntax

--- src/components/AddSubscriberForm/AddSubscriberForm.js
+++ src/components/AddSubscriberForm/AddSubscriberForm.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import { useForm } from "react-hook-form";
+
+import Button from "../Button";
+
+import "./index.css";
 
 function AddSubscriberForm({ onSubmit }) {
     const { register, handleSubmit, formState: { errors } } = useForm();
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form className="add-subscriber" onSubmit={handleSubmit(onSubmit)}>
+            <h2 className="form-header">Add subscriber</h2>
             <label htmlFor="name">Name: </label><input {...register("name", { required: "This field is required",  pattern: {
       value: /^[A-Z]/,
       message: 'Name must start with uppercase letter'
@@ -14,12 +20,17 @@ function AddSubscriberForm({ onSubmit }) {
             pattern: {
       value: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
       message: 'Email address is invalid'
-    } 
+    }
     })} type="email" />
     {errors.email && <p>{errors.email.message}</p>}
-            <button>Add</button>
+            <Button text="Add"/>
         </form>
     );
 }
 
 export default AddSubscriberForm;
+
+
+AddSubscriberForm.propTypes={
+    onSubmit: PropTypes.func.isRequired
+}
--- src/components/SubscriberList/SubscriberList.js
+++ src/components/SubscriberList/SubscriberList.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import PropTypes  from "prop-types";
+
+import {Link} from "react-router-dom";
+
 
 
 function SubscriberList({subscribers}) {
-    console.log(subscribers)
+    
+ 
     return (
         <div>
             <ul>
-                {subscribers.map(({fields,id})=><li key={id}>{fields.Name}; {fields.Email}</li>)}
+                {subscribers.map(({fields,id, createdTime})=><li key={id}><Link to={`/subscribers/${id}`}>{fields.Name}; {fields.Email}; {createdTime}</Link></li>)}
             </ul>
         </div>
     );
 }
 
 export default SubscriberList;
+
+
+SubscriberList.propTypes={
+    subscribers: PropTypes.array.isRequired
+}
